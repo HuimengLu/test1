e05591e2e1469d968f20c886ba8368a16990d6e8
HuimengLu <97484123+HuimengLu@users.noreply.github.com>
2022-02-03 23:55:34 -0800

    Starter+fail

---
+++ test1.java
@@ -0,0 +1,5 @@
+public class test1{
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ test1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class test1Test {
+    @Test
+    public void addition() {
+        assertEquals(2, test1.add(0, 1));
+    }
+}
